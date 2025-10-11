5f6f79605f85f998f17f376343408b0f8cdfa73f
Chris Nassif <chrisjnassif@gmail.com>
2025-10-10 21:36:35 -0400

    added reinforcement learning

--- donkey_env_lidar.py
+++ donkey_env_lidar.py
@@ -2,6 +2,8 @@ from gym_donkeycar.envs.donkey_env import DonkeyEnv
 import numpy as np
 from typing import Optional, Dict, Any, Tuple 
 from gymnasium import spaces
+import time
+
 
 class DonkeyEnvLidar(DonkeyEnv):
     """
@@ -11,6 +13,7 @@ class DonkeyEnvLidar(DonkeyEnv):
     def __init__(self, level: str, conf: Optional[Dict[str, Any]] = None):
         super().__init__(level, conf)
         
+        self.start_time = 0
         number_of_lidar_measurements = int(round(360/ conf["lidar_config"]["deg_per_sweep_inc"]))
         
         print(f"number of lidar measurements: {number_of_lidar_measurements}")
@@ -33,25 +36,31 @@ class DonkeyEnvLidar(DonkeyEnv):
             dtype=np.float32
         )
     
-    def compute_reward(self, info: dict, done: bool):
+    def compute_reward(self, info: dict):
         speed = info.get("speed", 0)
         cte = abs(info.get("cte", 0))
-        progress = info.get("progress", 0)
 
         # Encourage forward progress and speed
-        reward = speed * 0.1 + progress * 10.0
+        reward = info["forward_vel"] * 0.1
 
         # Penalize deviation from track center
         reward -= 0.05 * (cte ** 2)
 
         # Harsh penalty for leaving track or crashing
-        if done and cte > 2.0:
-            reward -= 100.0
+        if cte > 5.0:
+            reward -= 3
+        
+        if (info["hit"] != "none"):
+            reward -= 100
 
+        if (info["lap_count"] == 1):                    
+            reward += 50 - (time.time() - self.start_time)
+        
         return reward
 
 
     def step(self, action: np.ndarray) -> Tuple[np.ndarray, float, bool, Dict[str, Any]]:
+        
         camera_observation, reward, done, info = super().step(action)
         
         lidar_observation = self.viewer.handler.lidar
@@ -60,13 +69,16 @@ class DonkeyEnvLidar(DonkeyEnv):
         # Append the velocity and lidar observations together so the RL agent can see both of them
         full_observation = np.concatenate([lidar_observation, velocity_observation])
         
-        reward = self.compute_reward(info=info, done=done)
-        print(reward)
-        print("hi")
+        reward = self.compute_reward(info=info)
+        
+        done = done or info["lap_count"] == 1 or (time.time() - self.start_time) > 40 # if we complete the lap or we are in the environment for too long then we are done
+
         return full_observation, reward, done, done, info
     
     
     def reset(self, seed=None) -> tuple[np.ndarray, dict]:
+        
+        self.start_time = time.time()
         camera_observation = super().reset()
         
         lidar_observation = self.viewer.handler.lidar
